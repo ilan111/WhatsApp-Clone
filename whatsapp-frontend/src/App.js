7faddb22e7184e1ad330b518bae2a03865daaa8f
Ilan Y <ilanydg@gmail.com>
2020-11-15 01:42:39 +0200

    Initialized project Added firebase.js

---
+++ whatsapp-frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+     <h1 className="">This will be a MERN stack WhatsApp clone!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ whatsapp-frontend/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCdGBmMYJQt8RnlR6i1EVn-KColugDkkcs",
+    authDomain: "whatsapp-clone-mern-29c1f.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-mern-29c1f.firebaseio.com",
+    projectId: "whatsapp-clone-mern-29c1f",
+    storageBucket: "whatsapp-clone-mern-29c1f.appspot.com",
+    messagingSenderId: "325519561312",
+    appId: "1:325519561312:web:576e842eb9bac316c6a905"
+  };
