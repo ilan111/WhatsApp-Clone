3ce1311e9280f906d8bc384e85095d18536329bd
Ilan Y <ilanydg@gmail.com>
2020-11-15 03:04:39 +0200

    New components added (Chat and Sidebar)

---
+++ whatsapp-frontend/src/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Sidebar.css';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <div className="sidebar__headerRight">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
