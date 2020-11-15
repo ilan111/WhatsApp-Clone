229e237b166726ad93abc64e8fcc1cae6de54cda
Ilan Y <ilanydg@gmail.com>
2020-11-15 17:39:56 +0200

    Updated Chat component

--- whatsapp-frontend/src/Chat.js
+++ whatsapp-frontend/src/Chat.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import { Avatar, IconButton } from '@material-ui/core';
 import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import React from 'react'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
+
 import './Chat.css';
 
 function Chat() {
@@ -39,6 +42,37 @@ function Chat() {
                         {new Date().toUTCString()}
                     </span>
                 </p>
+                <p className="chat__message chat__sender">
+                    <span className="chat__name">
+                        Ilan Yadgarov
+                    </span>
+                    This is a message
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className="chat__message">
+                    <span className="chat__name">
+                        Ilan Yadgarov
+                    </span>
+                    This is a message
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
+
+            <div className="chat__footer">
+                <IconButton>
+                    <InsertEmoticonIcon/>
+                </IconButton>
+                <form action="">
+                    <input type="text" placeholder="Type a message"/>
+                    <button type="submit">Send a message</button>
+                </form>
+                <IconButton>
+                      <MicIcon/>
+                </IconButton>
             </div>
         </div>
 
