b703175b81557433668e5f2fa90ee37769f77397
Ilan Y <ilanydg@gmail.com>
2020-11-16 07:03:24 +0200

    Modified server.js file Added axios.js

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -62,6 +62,8 @@ db.once('open',()=>{
             pusher.trigger("messages","inserted",{
                 name: messageDetails.name,
                 message: messageDetails.message,
+                timestamp: messageDetails.timestamp,
+                received: messageDetails.received,
             })
         }
         else{
--- whatsapp-frontend/src/App.js
+++ whatsapp-frontend/src/App.js
@@ -1,8 +1,38 @@
-import './App.css';
+import React, {useEffect, useState} from 'react';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import Pusher from 'pusher-js';
+import axios from './axios';
+
+import './App.css';
 
 function App() {
+const [messages,setMessages] = useState([]);
+
+  useEffect(()=>{
+    axios.get('/messages/sync').then(response=>{
+      setMessages(response.data);
+    });
+  },[]);
+
+  useEffect(() => {
+    const pusher = new Pusher('80d6b942f4d697d76363', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(newMessage) {
+      setMessages([...messages, newMessage]);
+    });
+
+    return ()=>{
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
+
   return (
     <div className="app">    
       <div className="app__body">
